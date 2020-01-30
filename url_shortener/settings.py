17f107a1f33d45d0051ac0c58761b0545290f37c
DMK <dmkshift@gmail.com>
2020-01-30 15:41:07 +0200

    updated settings.py

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -75,13 +75,13 @@ WSGI_APPLICATION = 'url_shortener.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-    # 'default': dj_database_url.config(
-    #     default=config('DATABASE_URL')
-    # )
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 # Password validation
